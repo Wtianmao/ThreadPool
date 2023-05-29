9e419154ea4ea9c8d45c2c601ee98bda29424fe6
Wtianmao <100252189+Wtianmao@users.noreply.github.com>
2023-05-29 11:14:18 +0800

    Update ThreadPool.h

--- ThreadPool.h
+++ ThreadPool.h
@@ -16,7 +16,7 @@ public:
     ThreadPool(size_t);
     template<class F, class... Args>
     auto enqueue(F&& f, Args&&... args) 
-        -> std::future<typename std::result_of<F(Args...)>::type>;
+        -> std::future<decltype(f(args...))>>;
     ~ThreadPool();
 private:
     // need to keep track of threads so we can join them
@@ -43,7 +43,7 @@ inline ThreadPool::ThreadPool(size_t threads)
                     std::function<void()> task;
 
                     {
-                        std::unique_lock<std::mutex> lock(this->queue_mutex);
+                        std::lock_guard<std::mutex> lock(this->queue_mutex);
                         this->condition.wait(lock,
                             [this]{ return this->stop || !this->tasks.empty(); });
                         if(this->stop && this->tasks.empty())
@@ -61,9 +61,9 @@ inline ThreadPool::ThreadPool(size_t threads)
 // add new work item to the pool
 template<class F, class... Args>
 auto ThreadPool::enqueue(F&& f, Args&&... args) 
-    -> std::future<typename std::result_of<F(Args...)>::type>
+    -> std::future<decltype(f(args...))>>
 {
-    using return_type = typename std::result_of<F(Args...)>::type;
+    using return_type = decltype(f(args...));
 
     auto task = std::make_shared< std::packaged_task<return_type()> >(
             std::bind(std::forward<F>(f), std::forward<Args>(args)...)
@@ -71,7 +71,7 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
         
     std::future<return_type> res = task->get_future();
     {
-        std::unique_lock<std::mutex> lock(queue_mutex);
+        std::lock_guard<std::mutex> lock(queue_mutex);
 
         // don't allow enqueueing after stopping the pool
         if(stop)
